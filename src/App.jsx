d6740acd25cbf62e134029350eedf24e3a5cf98b
dalaodai <46028743+dalaodai@users.noreply.github.com>
2024-04-11 09:06:56 +0000

    no

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import { useState } from 'react'
 import './App.css'
 import Home from './components/home/Home'
 import About from './components/about/About'
 import Header from './components/header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
       <Header/>
-      <Home/>
-      <About/>
-      
+        <Home/>
+        <About/>
     </div>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,32 @@ import "./header.css"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand">Navbar</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" >Home page</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" >Features</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" >Pricing</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link disabled" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
   )
 }
 
